fdd0aa3aa7376f129425c2c5aeb73d078376deb6
Hari <hari@Haris-MacBook-Pro.local>
2023-03-28 15:30:10 +0530

    removed chars

--- src/har-to-mocks/features/write-mocks/utils/process-data.ts
+++ src/har-to-mocks/features/write-mocks/utils/process-data.ts
@@ -6,7 +6,7 @@ import type { Entry } from '../../../types';
 export const entrysToPathsWithData = (entrys: Entry[], targetPath: string) =>
   entrys.map((entry) => {
     const parsedUrl = new URL(entry.request.url);
-    const filePath = path.join(targetPath, parsedUrl.search || parsedUrl.pathname);
+    const filePath = path.join(targetPath, (parsedUrl.search || parsedUrl.pathname).replace(/[^\w\s]/gi, ''));
     const fileName = `${entry.request.method.toUpperCase()}.json`;
     const fileData = entry.response.content.text;
     return {
